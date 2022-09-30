796aa0d0ee60771e3981c37e8f96689ffe39cb01
Kang Young Gyu <kang.yg.dev@gmail.com>
2022-09-30 14:33:52 +0900

    Util 수정에 대한 추가 커밋

--- app/src/main/java/com/example/studyandroid/FragmentNavigationSub.kt
+++ app/src/main/java/com/example/studyandroid/FragmentNavigationSub.kt
@@ -3,6 +3,7 @@ package com.example.studyandroid
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.navArgs
+import com.example.studyandroid.Util.getStatusBarHeight
 import com.example.studyandroid.databinding.FragmentNavigationSubBinding
 
 class FragmentNavigationSub : BaseFragment<FragmentNavigationSubBinding>(FragmentNavigationSubBinding::inflate) {
@@ -13,7 +14,7 @@ class FragmentNavigationSub : BaseFragment<FragmentNavigationSubBinding>(Fragmen
 
         binding?.let {
             // StatusBar 높이만큼 패딩 적용.
-            it.clFragNavigationSub.setPadding(0, getStatusBarHeight(), 0, getStatusBarHeight())
+            it.clFragNavigationSub.setPadding(0, requireActivity().getStatusBarHeight(), 0, requireActivity().getStatusBarHeight())
 
             it.tvFragmentNavigationSubText.text = args.text
         }
