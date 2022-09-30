4009cdc824452fa53ab532a9094c25c18763fc86
Kang Young Gyu <kang.yg.dev@gmail.com>
2022-09-30 14:36:20 +0900

    Dialog 추가 - BaseDialog - FragmentNavigationDialog
    Navigation
    - Show DialogFragment
    - NavControll BackStackEntry

---
+++ app/src/main/java/com/example/studyandroid/BaseDialogFragment.kt
@@ -0,0 +1,40 @@
+package com.example.studyandroid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
+import com.example.studyandroid.Util.dpToPx
+import com.example.studyandroid.Util.getDeviceDisplayMetricsPoint
+
+abstract class BaseDialogFragment<VB : ViewBinding>(private val inflate: Inflate<VB>) : DialogFragment() {
+    private var _binding: VB? = null
+    val binding
+        get() = _binding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = inflate.invoke(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    protected fun setDialogSizeDp(widthDp: Int, heightDp: Int) {
+        dialog?.window?.setLayout(requireActivity().dpToPx(widthDp), requireActivity().dpToPx(heightDp))
+    }
+
+    protected fun setDialogSizeRatio(widthRatio: Float, heightRatio: Float = 1.0f) {
+        val devicePoint = requireActivity().getDeviceDisplayMetricsPoint()
+        val params = dialog?.window?.attributes?.apply {
+            width = (devicePoint.x * widthRatio).toInt()
+            if (heightRatio != 1.0f) height = (devicePoint.y * heightRatio).toInt()
+        }
+        dialog?.window?.attributes = params as WindowManager.LayoutParams
+    }
+}
--- app/src/main/java/com/example/studyandroid/FragmentNavigation.kt
+++ app/src/main/java/com/example/studyandroid/FragmentNavigation.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.studyandroid.Util.getStatusBarHeight
 import com.example.studyandroid.databinding.FragmentNavigationBinding
 
 class FragmentNavigation : BaseFragment<FragmentNavigationBinding>(FragmentNavigationBinding::inflate) {
@@ -12,16 +14,21 @@ class FragmentNavigation : BaseFragment<FragmentNavigationBinding>(FragmentNavig
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        navController = Navigation.findNavController(view)
-
+        navController = findNavController()
         binding?.let {
             // StatusBar 높이만큼 패딩 적용.
-            it.clFragNavigation.setPadding(0, getStatusBarHeight(), 0, getStatusBarHeight())
+            it.clFragNavigation.setPadding(0, requireActivity().getStatusBarHeight(), 0, requireActivity().getStatusBarHeight())
+
+            navController.currentBackStackEntry?.savedStateHandle?.getLiveData<String>("text")?.observe(viewLifecycleOwner) { str ->
+                it.tvCurrentBackStack.text = str
+            }
 
             it.btGoFragmentNavigationSub.setOnClickListener {
                 val action = FragmentNavigationDirections.actionFragmentNavigationToFragmentNavigationSub("Apple")
                 navController.navigate(action)
             }
+
+            it.btGoFragmentNavigationDialog.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_fragmentNavigation_to_fragmentNavigationDialog, null))
         }
     }
 }
