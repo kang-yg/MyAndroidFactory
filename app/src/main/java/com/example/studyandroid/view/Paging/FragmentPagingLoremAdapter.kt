0e6ac293d844603bd0ea4fe43e732c255467d21f
Kang Young Gyu <kang.yg.dev@gmail.com>
2022-11-22 10:53:07 +0900

    * FragmentPaging - Apply Shimmer to recyclerview item ImageView

--- app/src/main/java/com/example/studyandroid/view/Paging/FragmentPagingLoremAdapter.kt
+++ app/src/main/java/com/example/studyandroid/view/Paging/FragmentPagingLoremAdapter.kt
@@ -8,13 +8,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.studyandroid.Util.dpToPx
 import com.example.studyandroid.databinding.FragmentPagingLoremItemBinding
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 
 class FragmentPagingLoremAdapter : PagingDataAdapter<LoremImageInfo, FragmentPagingLoremAdapter.FragmentPagingLoremViewHolder>(diffUtil) {
     class FragmentPagingLoremViewHolder(private val viewBinding: FragmentPagingLoremItemBinding) : RecyclerView.ViewHolder(viewBinding.root) {
+        private val shimmerDrawable = ShimmerDrawable().apply {
+            setShimmer(
+                Shimmer.AlphaHighlightBuilder()
+                    .setDuration(1500)
+                    .setBaseAlpha(0.8f)
+                    .setHighlightAlpha(0.4f)
+                    .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+                    .setAutoStart(true)
+                    .build()
+            )
+        }
+
         fun bind(loremImageInfo: LoremImageInfo) {
             with(viewBinding) {
                 val context = root.context
-                Glide.with(context).load(loremImageInfo.downloadUrl).override(context.dpToPx(250), context.dpToPx(200)).centerCrop().into(ivLorem)
+                Glide.with(context).load(loremImageInfo.downloadUrl).placeholder(shimmerDrawable).override(context.dpToPx(250), context.dpToPx(200)).centerCrop().into(ivLorem)
                 tvLoremAuthor.text = loremImageInfo.author
             }
         }
