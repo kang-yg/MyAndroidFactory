e75303a911b7b2a7cf30dd57497ad8494d30f108
Kang Young Gyu <kang.yg.dev@gmail.com>
2023-05-03 17:26:08 +0900

    RequestPermission Util로 수정

--- app/src/main/java/com/example/studyandroid/Util.kt
+++ app/src/main/java/com/example/studyandroid/Util.kt
@@ -1,8 +1,15 @@
 package com.example.studyandroid
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.graphics.Point
 import android.util.TypedValue
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 
 object Util {
     /**
@@ -18,4 +25,32 @@ object Util {
     fun Context.pxToDp(scale: Int): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, scale.toFloat(), resources.displayMetrics).toInt()
 
     fun Context.getDeviceDisplayMetricsPoint(): Point = Point(resources.displayMetrics.widthPixels, resources.displayMetrics.heightPixels)
+
+    fun requestPermissionLauncher(activity: ComponentActivity? = null, fragment: Fragment? = null): ActivityResultLauncher<Array<String>>? {
+        return activity?.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+
+        } ?: fragment?.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+
+        }
+    }
+
+    fun checkPermission(activity: Activity, permissions: Array<String>): Boolean {
+        if (permissions.none { ActivityCompat.shouldShowRequestPermissionRationale(activity, it) })
+            return true
+
+        permissions.forEach {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, it)) {
+                showPermissionAlert(activity, it)
+            }
+        }
+        return false
+    }
+
+    private fun showPermissionAlert(context: Context, permissionName: String): AlertDialog {
+        return AlertDialog.Builder(context)
+            .setTitle("Alert")
+            .setMessage("$permissionName 권한이 필요 합니다.")
+            .setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+            .create()
+    }
 }
--- app/src/main/java/com/example/studyandroid/view/BaseFragment.kt
+++ app/src/main/java/com/example/studyandroid/view/BaseFragment.kt
@@ -1,11 +1,9 @@
 package com.example.studyandroid.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 
@@ -23,11 +21,5 @@ abstract class BaseFragment<VB : ViewBinding>(private val inflate: Inflate<VB>) 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    fun requestPermissionLauncher() = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-        permissions.forEach {
-            Log.d("Permission: ", "${it.key} is ${it.value}")
-        }
     }
 }
